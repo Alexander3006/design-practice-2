f5c38585a4ee738a84116e17391ef2dd2ffc149c
G1gg1L3s <mr.a678i@gmail.com>
2021-05-19 17:37:22 +0300

    Add db and segment tests

--- cmd/datastore/db_test.go
+++ cmd/datastore/db_test.go
@@ -3,32 +3,28 @@ package datastore
 import (
 	"io/ioutil"
 	"os"
-	"path/filepath"
 	"testing"
 )
 
+const segmentSize = 10240
+
 func TestDb_Put(t *testing.T) {
-	dir, err := ioutil.TempDir("", "test-db")
+	dir, err := ioutil.TempDir(".", "test-db-*")
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer os.RemoveAll(dir)
 
-	db, err := NewDb(dir)
+	db, err := NewDb(dir, segmentSize)
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer db.Close()
 
-	pairs := [][]string {
+	pairs := [][]string{
 		{"key1", "value1"},
 		{"key2", "value2"},
 		{"key3", "value3"},
-	}
-
-	outFile, err := os.Open(filepath.Join(dir, outFileName))
-	if err != nil {
-		t.Fatal(err)
 	}
 
 	t.Run("put/get", func(t *testing.T) {
@@ -47,33 +43,9 @@ func TestDb_Put(t *testing.T) {
 		}
 	})
 
-	outInfo, err := outFile.Stat()
-	if err != nil {
-		t.Fatal(err)
-	}
-	size1 := outInfo.Size()
-
-	t.Run("file growth", func(t *testing.T) {
-		for _, pair := range pairs {
-			err := db.Put(pair[0], pair[1])
-			if err != nil {
-				t.Errorf("Cannot put %s: %s", pairs[0], err)
-			}
-		}
-		outInfo, err := outFile.Stat()
-		if err != nil {
-			t.Fatal(err)
-		}
-		if size1 * 2 != outInfo.Size() {
-			t.Errorf("Unexpected size (%d vs %d)", size1, outInfo.Size())
-		}
-	})
-
 	t.Run("new db process", func(t *testing.T) {
-		if err := db.Close(); err != nil {
-			t.Fatal(err)
-		}
-		db, err = NewDb(dir)
+		db.Close()
+		db, err = NewDb(dir, segmentSize)
 		if err != nil {
 			t.Fatal(err)
 		}
---
+++ cmd/datastore/segment_test.go
@@ -0,0 +1,72 @@
+package datastore
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+// We want to crate 2 segments - minimum number before segments start to merge.
+// To do this, we set the segment to be 1 KB and write 2 KB of entries.
+// Our keys and values both have length of 10 bytes. Together with an
+// entry header it should be 32 bytes per entry.
+const KB = 1024
+const ENTRY = 32
+const ENTRY_NUMBER = 2 * KB / ENTRY
+
+// Craft 10 bytes wide string
+func craft_string(i int) string {
+	return fmt.Sprintf("%010d", i)
+}
+
+func Test_Segment(t *testing.T) {
+	dir, err := ioutil.TempDir(".", "test-segment-*")
+	if err != nil {
+		t.Fatal("Creating db store error")
+	}
+	defer os.RemoveAll(dir)
+
+	db, err := NewDb(dir, KB)
+	if err != nil {
+		t.Fatal("Creating db error", err)
+	}
+
+	for i := 0; i < ENTRY_NUMBER; i++ {
+		key := craft_string(i)
+		err := db.Put(key, key)
+		if err != nil {
+			println(err.Error())
+			t.Errorf("Put db error: %d", i)
+		}
+	}
+
+	for i := 0; i < ENTRY_NUMBER; i++ {
+		key := craft_string(i)
+		val, err := db.Get(key)
+		if err != nil {
+			t.Errorf("Get db error: %d", i)
+		}
+		if val != key {
+			t.Errorf("Compare val error: %d", i)
+			if err != ErrNotFound {
+				t.Errorf("%s !== %s", val, key)
+			}
+		}
+	}
+
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			t.Errorf("Walking error: %s", err)
+		}
+		// skip "."
+		if info.IsDir() {
+			return nil
+		}
+		if info.Size() > KB {
+			t.Errorf("segment %s: size is %d, but expected to be less than %d", path, info.Size(), KB)
+		}
+		return nil
+	})
+}
